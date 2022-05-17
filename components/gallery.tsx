9c3518a34ad1ca1b95b2da3610cd3a8759871a26
Devin Carter <devinacarter97@gmail.com>
2022-05-17 17:27:31 +0100

    Added images

--- components/gallery.tsx
+++ components/gallery.tsx
@@ -1,10 +1,48 @@
-import { ArrowForwardIcon } from '@chakra-ui/icons'
-import { Divider, Stack, Flex, Text, Box, Image } from '@chakra-ui/react'
-import { Button, ButtonGroup } from '@chakra-ui/react'
+import { ArrowForwardIcon } from "@chakra-ui/icons";
+import { Divider, Stack, Flex, Text, Box, Image, Container } from "@chakra-ui/react";
+import { Button, ButtonGroup } from "@chakra-ui/react";
 
 const Gallery = () => {
-    return (
-        <div>hello</div>
-    )
-}
-export default Gallery
+  return (
+    <Container
+    mt='4'
+        display='inline-flex'
+        alignItems='center'
+        justifyContent='center'
+        width='100%'
+        objectFit={'contain'}    
+        >
+        <Image
+        boxSize="212px"
+        src="https://bit.ly/dan-abramov"
+        alt="Dan Abramov"
+      />
+      <Image
+        boxSize="212px"
+        src="https://bit.ly/dan-abramov"
+        alt="Dan Abramov"
+      />
+      <Image
+        boxSize="212px"
+        src="https://bit.ly/dan-abramov"
+        alt="Dan Abramov"
+      />
+      <Image
+        boxSize="212px"
+        src="https://bit.ly/dan-abramov"
+        alt="Dan Abramov"
+      />
+      <Image
+        boxSize="212px"
+        src="https://bit.ly/dan-abramov"
+        alt="Dan Abramov"
+      />
+      <Image
+        boxSize="212px"
+        src="https://bit.ly/dan-abramov"
+        alt="Dan Abramov"
+      />
+    </Container>
+  );
+};
+export default Gallery;
