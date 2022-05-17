ff811bc26a5a4b3a2b1527d9da7315d14058f3a0
lapsha-dev <jzck@riseup.net>
2022-05-17 15:12:01 +0100

    complete nav

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,21 +1,51 @@
-import { Flex, IconButton, Menu, MenuButton, MenuList, MenuItem } from "@chakra-ui/react";
-import { HamburgerIcon } from "@chakra-ui/icons";
+import {
+  Flex,
+  IconButton,
+  Menu,
+  MenuButton,
+  MenuList,
+  MenuItem,
+  Button,
+  Text,
+  Box,
+  HStack,
+  useColorMode,
+  Spacer,
+} from "@chakra-ui/react";
+import { HamburgerIcon, MoonIcon, SunIcon } from "@chakra-ui/icons";
 
 const Navbar = () => {
+  const { colorMode, toggleColorMode } = useColorMode();
   return (
-    <Flex p='10px'>
-      <Menu>
-        <MenuButton
-          as={IconButton}
-          aria-label="Dropdown menu"
-          icon={<HamburgerIcon />}
-        ></MenuButton>
-        <MenuList>
+    <Flex align="center" justify={"space-between"}>
+      <Text fontSize='xl' textAlign="center" display={{ base: "none", md: "flex" }}>
+        Logo Here
+      </Text>
+      <Box display={{ base: "flex", md: "none" }}>
+        <Menu>
+          <MenuButton
+            as={IconButton}
+            aria-label="Dropdown menu"
+            icon={<HamburgerIcon />}
+          ></MenuButton>
+          <MenuList>
             <MenuItem>About</MenuItem>
             <MenuItem>Roadmap</MenuItem>
             <MenuItem>Team</MenuItem>
-        </MenuList>
-      </Menu>
+          </MenuList>
+        </Menu>
+      </Box>
+      <HStack spacing='100px' justify={"space-between"} display={{base: 'none', md:'flex'}} >
+          <Button variant='ghost'>About</Button>
+          <Button variant='ghost'>Roadmap</Button>
+          <Button variant='ghost'>Team</Button>
+      </HStack>
+      <HStack spacing='15px'>
+        <Button>Connect Wallet</Button>
+        <IconButton aria-label="Theme switcher" onClick={toggleColorMode}>
+          {colorMode === "light" ? <MoonIcon /> : <SunIcon />}
+        </IconButton>
+      </HStack>
     </Flex>
   );
 };
