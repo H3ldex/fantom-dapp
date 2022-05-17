5925ac207f6e97a48a97b4111e0dd448af5f2944
lapsha-dev <jzck@riseup.net>
2022-05-17 14:05:55 +0100

    start navbar

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,9 +1,23 @@
-import { useState } from "react"
+import { Flex, IconButton, Menu, MenuButton, MenuList, MenuItem } from "@chakra-ui/react";
+import { HamburgerIcon } from "@chakra-ui/icons";
 
-const navbar = () => {
+const Navbar = () => {
   return (
-    <div>navbar</div>
-  )
-}
+    <Flex p='10px'>
+      <Menu>
+        <MenuButton
+          as={IconButton}
+          aria-label="Dropdown menu"
+          icon={<HamburgerIcon />}
+        ></MenuButton>
+        <MenuList>
+            <MenuItem>About</MenuItem>
+            <MenuItem>Roadmap</MenuItem>
+            <MenuItem>Team</MenuItem>
+        </MenuList>
+      </Menu>
+    </Flex>
+  );
+};
 
-export default navbar
+export default Navbar;
