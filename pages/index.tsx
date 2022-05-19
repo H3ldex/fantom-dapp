9956bfa878f3fed3ca6724998804d24b2c10c1e9
lapsha-dev <jzck@riseup.net>
2022-05-19 16:06:52 +0100

    rainbowkit wallet setup

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,22 +1,73 @@
 import type { NextPage } from "next";
-import Navbar from "../components/navbar"
-import Hero from "../components/hero"
-import Team from "../components/team"
-import Gallery from "../components/gallery"
-import Roadmap from "../components/roadmap"
+import Navbar from "../components/navbar";
+import Hero from "../components/hero";
+import Team from "../components/team";
+import Gallery from "../components/gallery";
+import Roadmap from "../components/roadmap";
 import { Box, Container } from "@chakra-ui/react";
+
+import "@rainbow-me/rainbowkit/styles.css";
+import {
+  apiProvider,
+  Chain,
+  configureChains,
+  getDefaultWallets,
+  RainbowKitProvider,
+} from "@rainbow-me/rainbowkit";
+import { chain, createClient, WagmiProvider } from "wagmi";
+
+const operaChain: Chain = {
+  id: 250,
+  name: "Fantom Opera",
+  nativeCurrency: {
+    decimals: 18,
+    name: "Fantom",
+    symbol: "FTM",
+  },
+  rpcUrls: {
+    default: "https://rpc.ftm.tools/",
+  },
+  blockExplorers: {
+    default: { name: "FTMScan", url: "https://ftmscan.com/" },
+    ftmscan: { name: "FTMScan", url: "https://ftmscan.com/" },
+  },
+  testnet: false,
+};
+
+const { chains, provider } = configureChains(
+  [chain.mainnet],
+  [
+    apiProvider.alchemy(process.env.ALCHEMY_ID),
+    apiProvider.jsonRpc((chain) => ({ rpcUrl: chain.rpcUrls.default })),
+  ]
+);
+
+const { connectors } = getDefaultWallets({
+  appName: "Fantom Dapp",
+  chains,
+});
+
+const wagmiClient = createClient({
+  autoConnect: true,
+  connectors,
+  provider,
+});
 
 const Home: NextPage = () => {
   return (
-    <Box>
-      <Container pt='15px' maxWidth='1280px'>
-      <Navbar/>
-      <Hero/>
-      <Gallery/>
-      <Team/>
-      <Roadmap/>
-      </Container>
-    </Box>
+    <WagmiProvider client={wagmiClient}>
+      <RainbowKitProvider chains={chains}>
+        <Box>
+          <Container pt="15px" maxWidth="1280px">
+            <Navbar />
+            <Hero />
+            <Gallery />
+            <Team />
+            <Roadmap />
+          </Container>
+        </Box>
+      </RainbowKitProvider>
+    </WagmiProvider>
   );
 };
 
