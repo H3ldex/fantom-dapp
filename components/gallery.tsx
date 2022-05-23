92e2f334e3edfa5de4ca14c7bd00cdeea228158f
lapsha-dev <jzck@riseup.net>
2022-05-23 22:24:47 +0100

    fix responsiveness

--- components/gallery.tsx
+++ components/gallery.tsx
@@ -1,35 +1,39 @@
-import {
-  Flex,
-  Spacer,
-  Box,
-} from "@chakra-ui/react";
+import { Flex, Spacer, Box, Container, Center } from "@chakra-ui/react";
 import GalleryImage from "./galleryImages";
 
 const Gallery = () => {
   return (
-    <Flex
-      flexDir={{ base: "column", md: "row" }}
-      alignItems="center"
-      justify="center"
-      mt="20px"
-      mb='20px'
-    >
-      <GalleryImage url="0.png" altname="Punk0 NFT" rotation="rotate(5deg)" />
-      <Spacer />
-      <GalleryImage url="1.png" altname="Punk1 NFT" rotation="rotate(-5deg)" />
-      <Spacer />
-      <GalleryImage url="2.png" altname="Punk2 NFT" rotation="rotate(5deg)" />
-      <Spacer />
-      <Box display={{ base: 'none', md: 'flex'}}>
+      <Flex
+        flexDir={{ base: "column", md: "row" }}
+        alignItems="center"
+        justify="center"
+        mt="20px"
+        mb="20px"
+      >
+        <GalleryImage url="0.png" altname="Punk0 NFT" rotation="rotate(5deg)" />
+        <Spacer />
         <GalleryImage
-          url="3.png"
-          altname="Punk2 NFT"
+          url="1.png"
+          altname="Punk1 NFT"
           rotation="rotate(-5deg)"
         />
         <Spacer />
-        <GalleryImage url="4.png" altname="Punk2 NFT" rotation="rotate(5deg)" />
-      </Box>
-    </Flex>
+        <GalleryImage url="2.png" altname="Punk2 NFT" rotation="rotate(5deg)" />
+        <Spacer />
+        <Box display={{ base: "none", xl: 'flex'}}>
+          <GalleryImage
+            url="3.png"
+            altname="Punk2 NFT"
+            rotation="rotate(-5deg)"
+          />
+          <Spacer />
+          <GalleryImage
+            url="4.png"
+            altname="Punk2 NFT"
+            rotation="rotate(5deg)"
+          />
+        </Box>
+      </Flex>
   );
 };
 export default Gallery;
