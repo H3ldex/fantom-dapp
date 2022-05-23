38d7c04e9f534b8d49a5178424bafc9740ace6f3
lapsha-dev <jzck@riseup.net>
2022-05-23 14:04:18 +0100

    start themeing rainbowkit button

---
+++ themes/rkTheme.tsx
@@ -0,0 +1,54 @@
+import { Theme } from '@rainbow-me/rainbowkit';
+
+const rkTheme: Theme = {
+  colors: {
+    accentColor: '...',
+    accentColorForeground: '#000',
+    actionButtonBorder: '#c3b1e1',
+    actionButtonBorderMobile: '...',
+    actionButtonSecondaryBackground: '...',
+    closeButton: '...',
+    closeButtonBackground: '...',
+    connectButtonBackground: '...',
+    connectButtonBackgroundError: '...',
+    connectButtonInnerBackground: '#fff',
+    connectButtonText: '...',
+    connectButtonTextError: '...',
+    connectionIndicator: '...',
+    error: '...',
+    generalBorder: '#000',
+    generalBorderDim: '...',
+    menuItemBackground: '#fff',
+    modalBackdrop: '...',
+    modalBackground: '#C3B1E1',
+    modalBorder: '#000',
+    modalText: '...',
+    modalTextDim: '...',
+    modalTextSecondary: '...',
+    profileAction: '...',
+    profileActionHover: '...',
+    profileForeground: '#c3b1e1',
+    selectedOptionBorder: '...',
+    standby: '...',
+  },
+  fonts: {
+    body: 'Permanent Marker',
+  },
+  radii: {
+    actionButton: '...',
+    connectButton: '...',
+    menuButton: '...',
+    modal: '...',
+    modalMobile: '...',
+  },
+  shadows: {
+    connectButton: '...',
+    dialog: '...',
+    profileDetailsAction: '...',
+    selectedOption: '...',
+    selectedWallet: '...',
+    walletLogo: '...',
+  },
+};
+
+export default rkTheme
